eb705d279400e73dbd10878a49bccccac1c789c2
mostafaabdelaal <moustaphaabdelaal010@gmail.com>
2024-09-09 18:16:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Use Python 3.8 slim as the base image
+FROM python:3.8-slim
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the current directory contents into the container
+COPY . /app
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Command to run the application
+#CMD ["python", "main.py"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
